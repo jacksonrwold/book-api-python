036cd897de13493fbb95d8a3c17cd4c3cfa518ae
jacksonrwold <jacksonrwold@gmail.com>
2019-03-28 14:15:40 -0700

    commit

--- app.py
+++ app.py
@@ -44,6 +44,16 @@ def return_books():
     all_books = db.session.query(Books.id, Books.title, Books.author).all()
     return jsonify(all_books)
 
+@app.route('/book/<id>', methods=['GET'])
+def return_single_book(id):
+    one_book = db.session.query(Books.id, Books.title, Books.author).filter(Books.id == id).first()
+    return jsonify(one_book)
+    """
+    SELECT books.title, etc 
+    FROM books
+    WHERE books.id = 1
+    """
+
 @app.route('/delete/<id>', methods=["DELETE"])
 def book_delete(id):
     if request.content_type == 'application/json':
@@ -52,6 +62,19 @@ def book_delete(id):
         db.session.commit()
         return jsonify("Completed Delete action")
     return jsonify("Delete Failed")
+
+@app.route('/update_book/<id>', methods=["PUT"])
+def book_update(id):
+    if request.content_type == 'application/json':
+        put_data = request.get_json()
+        title = put_data.get('title')
+        author = put_data.get('author')
+        record = db.session.query(Books).get(id)
+        record.title = title
+        record.author = author
+        db.session.commit()
+        return jsonify("Completed Update")
+    return jsonify("Update Failed")
 
 if __name__ == '__main__':
     app.debug = True
